dde9c221ab5f4c25773a7c721d69e3579f74808f
Michael <michael.spengler@sap.com>
2021-06-09 00:53:08 +0200

    successfully using nlp without a dedicated server

--- src/components/addnewdeck/ImportDeckFromURL.vue
+++ src/components/addnewdeck/ImportDeckFromURL.vue
@@ -26,7 +26,12 @@
 import Vue from "vue";
 import Component from "vue-class-component";
 // import NlpManager from "node-nlp";
-const { NlpManager } = require('node-nlp');
+
+const { containerBootstrap } = require("@nlpjs/core");
+const { Nlp } = require("@nlpjs/nlp");
+const { LangEn } = require("@nlpjs/lang-en-min");
+
+// const { NlpManager } = require('node-nlp');
 
 import { Event } from "../../types";
 
@@ -39,38 +44,46 @@ export default class ImportDeckFromURL extends Vue {
       new RegExp("^https://.*/.*.json$").exec(value) !== null ||
       "Please provide a correct URL",
   ];
-  async loadFileFromURL() {
+
+  async trainChatbot(fileContent: any) {
+
     alert("hi");
 
-    const manager = new NlpManager({ languages: ["en"], forceNER: true });
-    // Adds the utterances and intents for the NLP
-    manager.addDocument("en", "goodbye for now", "greetings.bye");
-    manager.addDocument("en", "bye bye take care", "greetings.bye");
-    manager.addDocument("en", "okay see you later", "greetings.bye");
-    manager.addDocument("en", "bye for now", "greetings.bye");
-    manager.addDocument("en", "i must go", "greetings.bye");
-    manager.addDocument("en", "hello", "greetings.hello");
-    manager.addDocument("en", "hi", "greetings.hello");
-    manager.addDocument("en", "howdy", "greetings.hello");
-
-    // Train also the NLG
-    manager.addAnswer("en", "greetings.bye", "Till next time");
-    manager.addAnswer("en", "greetings.bye", "see you soon!");
-    manager.addAnswer("en", "greetings.hello", "Hey there!");
-    manager.addAnswer("en", "greetings.hello", "Greetings!");
-
-    // Train and save the model.
     (async () => {
-      await manager.train();
-      manager.save();
-      const response = await manager.process("en", "I should go now");
-      console.log(response);
-      alert(response)
-    })();
+      const container = await containerBootstrap();
+      container.use(Nlp);
+      container.use(LangEn);
+      const nlp = container.get("nlp");
+      nlp.settings.autoSave = false;
+      nlp.addLanguage("en");
+      // Adds the utterances and intents for the NLP
+      nlp.addDocument("en", "goodbye for now", "greetings.bye");
+      nlp.addDocument("en", "bye bye take care", "greetings.bye");
+      nlp.addDocument("en", "okay see you later", "greetings.bye");
+      nlp.addDocument("en", "bye for now", "greetings.bye");
+      nlp.addDocument("en", "i must go", "greetings.bye");
+      nlp.addDocument("en", "hello", "greetings.hello");
+      nlp.addDocument("en", "hi", "greetings.hello");
+      nlp.addDocument("en", "howdy", "greetings.hello");
 
+      // Train also the NLG
+      nlp.addAnswer("en", "greetings.bye", "Till next time");
+      nlp.addAnswer("en", "greetings.bye", "see you soon!");
+      nlp.addAnswer("en", "greetings.hello", "Hey there!");
+      nlp.addAnswer("en", "greetings.hello", "Greetings!");
+      await nlp.train();
+      const response = await nlp.process("en", "I should go now");
+      console.log(response);
+    })();
+  }
+
+  async loadFileFromURL() {
     try {
       const response = await fetch(this.chosenURL);
       const fileContent = await response.json();
+
+      await this.trainChatbot(fileContent);
+      
       this.$eventHub.$emit(Event.ADD_DECKS_FROM_JSON, fileContent, this.chosenURL);
     } catch (error) {
       // console.log(error);
